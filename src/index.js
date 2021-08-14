2ae6b6f7cc0c4c5af2cd4abad689893cd854421b
Husain Shahid Rao <husain@pop-os.localdomain>
2021-08-14 13:38:07 +0530

    debug

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-import "./css/style.css"
-import "./css/footer.css"
+// import "./css/style.css"
+// import "./css/footer.css"
 
 ReactDOM.render(<App />, document.getElementById("root"));
