5b7b30bc82193a5d76ccf32f527a6eb04479fab6
Husain Shahid Rao <husain@pop-os.localdomain>
2021-08-31 15:04:33 +0530

    debugging

--- src/pages/Calculators.jsx
+++ src/pages/Calculators.jsx
@@ -2,16 +2,22 @@ import React from "react";
 import RetirementCalculator from "../components/Calculator/RetirementCalculator";
 import styled from "styled-components";
 const CaluculatorPage = styled.div`
-padding: 25px 0;
-background: url(/assets/calcWave.svg) no-repeat;
-background-size: cover;
-background-color: #04009A;
+  padding: 25px 0;
+  background: url(/assets/calcWave.svg) no-repeat;
+  background-size: cover;
+  background-color: #04009a;
+  h1 {
+    color: #fff;
+    font-size: 2.5rem;
+    text-align: center;
+    margin-bottom: 25px;
+  }
 `;
-
 
 const Calculators = () => {
   return (
     <CaluculatorPage>
+      <h1>Retirement Calculator</h1>
       <RetirementCalculator />
     </CaluculatorPage>
   );
